3bac175f5e010702aecfb0d048d0be31fa05e9c4
farizdotid <contact.farizramadhan@gmail.com>
2024-07-19 23:23:25 +0700

    first commit :tada:

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,14 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
+  
     colors: {
       "griko": "#27292b",
       "cultured": "#f9f9f9",
-      "flash": "#ffc24f"
+      "flash": "#ffc24f",
+      "bluelight": "#85C1E9",
+      "black": "#000000",
+      "pendopo": "#C1A14E",
     },
     fontFamily: {
       sans: ["Inter", fontFamily.sans],
